82245285c2dea2ad53b783f4cc2e2a2961510c7e
DHARMIK  GANGANI <dharmikgangani786@gmail.com>
2025-09-28 17:51:01 -0300

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,1 +1,15 @@
 # file
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.100.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
+}
